35632b41b926e67b06b51b8d48413fe617b8c3ab
Geoff <geoffreylaporte@gmail.com>
2021-01-21 14:52:47 -0600

    updated characters jsx file with base code

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Screens/Home/Home";
 import Movies from "./Screens/Movies/Movies";
+import Characters from "./Screens/Characters/Characters";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path='/characters' component={Characters} />
         <Route exact path="/movies" component={Movies} />
       </Switch>
     </div>
