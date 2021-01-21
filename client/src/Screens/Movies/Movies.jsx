a73bd5e1bea5130ffccf82da3344c64dc1b0b35e
Geoff <geoffreylaporte@gmail.com>
2021-01-21 14:37:02 -0600

    added cors, updated App,js and completed get request for movies

--- client/src/Screens/Movies/Movies.jsx
+++ client/src/Screens/Movies/Movies.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllMovies} from '../../Services/movies';
 
 function Movies(props) {
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const movieData = await getAllMovies()
+      setMovies(movieData)
+    }
+    fetchData()
+  }, [])
+
+
   return (
     <div>
-      
+
     </div>
   );
 }
