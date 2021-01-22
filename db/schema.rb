018e6fe790550232cab0290eb3d19e0f971f6432
Geoff <geoffreylaporte@gmail.com>
2021-01-22 10:12:02 -0600

    updated schema for characters to include image_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_182218) do
+ActiveRecord::Schema.define(version: 2021_01_22_160757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_182218) do
     t.string "accessory"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_URL"
   end
 
   create_table "characters_movies", id: false, force: :cascade do |t|
